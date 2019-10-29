256649c679486e352c548e4415934cfa7d395f94
Carsonoid <ca@carsonoid.net>
2019-10-28 19:31:27 -0600

    Update yaml demo

--- wigm-yaml/demo.sh
+++ wigm-yaml/demo.sh
@@ -7,78 +7,57 @@ export DEMO_COMMENT_COLOR=$CYAN
 
 export KUBECTL="kubectl --kubeconfig=kubeconfig.yaml"
 
-# Create first release
-p "# Do release 1, with DEFAULT business decisions"
-pe "cp resources.yaml releases/yamlrelease.yaml"
-p "sed -i s'|\\\bNAME\\\b|yamlrelease|' releases/yamlrelease.yaml"
-sed -i s'|\bNAME\b|yamlrelease|' releases/yamlrelease.yaml
+# Create New Instance
+p "# Create new instance with YAML"
+pe "cp resources.yaml releases/yaml1.yaml"
+p "sed -i s'|\\\bNAME\\\b|yaml1|' releases/yaml1.yaml"
+sed -i s'|\bNAME\b|yaml1|' releases/yaml1.yaml
+p "sed -i s'|\\\bTITLE\\\b|yaml1|' releases/yaml1.yaml"
+sed -i s'|\bTITLE\b|yaml1|' releases/yaml1.yaml
+pe "sed -i s'|value: LINK|value: https://media.giphy.com/media/JloRBVCgTP6RW/giphy.gif|' releases/yaml1.yaml"
 wait
-pe "sed -i s'|value: LINK|value: https://media.giphy.com/media/JloRBVCgTP6RW/giphy.gif|' releases/yamlrelease.yaml"
+p "# Enable cloud LB with manual edits"
+p "vim ./releases/yaml1.yaml"
 wait
-pe "$KUBECTL apply -f releases/yamlrelease.yaml"
+p "# Disable Ingress with manual edits"
+p "vim ./releases/yaml1.yaml"
 wait
-
-
-# Create second release
-p "# Do release 2, with INVERTED business decisions"
-pe "cp resources.yaml releases/holdem.yaml"
-p "sed -i s'|\\\bNAME\\\b|holdem|' releases/holdem.yaml"
-sed -i s'|\bNAME\b|holdem|' releases/holdem.yaml
-wait
-pe "sed -i s'|value: LINK|value: https://media.giphy.com/media/l2JehUhBRqoJXC6t2/giphy.gif|' releases/holdem.yaml"
-wait
-p "# Now go edit releases/holdem.yaml and uncomment the type line in the service"
-p "# Then delete the ingress section"
-wait
-pe "$KUBECTL apply -f releases/holdem.yaml"
-wait
-
-# ----------------------------- Curl the ingress
-p "# Curl the cluster ingress controller with our wigm hostname"
-pe "NODEIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' wigm-yaml_node_1)"
-pe "curl -s -H 'Host: yamlrelease.wigm.carson-anderson.com' $NODEIP"
+pe "$KUBECTL apply -f releases/yaml1.yaml"
 wait
 
 # ----------------------------- Proxy
-p "# Proxy connection into a release 1 pod, Control-C to kill and continue"
-pe "$KUBECTL port-forward svc/wigm-host-yamlrelease 8888:80"
+p "# Proxy http://localhost:8888 into pod"
+p "# Control-C to kill and continue"
+pe "$KUBECTL port-forward svc/wigm-host-yaml1 8888:80"
 
-# ----------------------------- Upgrade
-p "# Upgrade release 1. Set a better name, disable ingress"
+# ----------------------------- Update Title
+p "# Update title"
 p "# Since this release is raw yaml, you have to"
 p "# edit in place with 'kubectl edit'"
 p "# or"
 p "# edit the release file and 'kubectl apply'"
-p '# Press enter to continue'
+p "vim ./releases/yaml1.yaml"
 p ''
 wait
 
-# ----------------------------- Curl the ingress
-p "# Curl the cluster ingress controller with our wigm hostname"
-pe "NODEIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' wigm-yaml_node_1)"
-pe "curl -s -H 'Host: yamlrelease.wigm.carson-anderson.com' $NODEIP"
+# ----------------------------- Enable Ingress
+p "# Enable ingress"
+p "# Go... find that code... I guess... and copy it back in"
+p "vim ./releases/yaml1.yaml"
 wait
 
 # ----------------------------- Proxy
-p "# Proxy connection into a release 1 pod, Control-C to kill and continue"
-pe "$KUBECTL port-forward svc/wigm-host-yamlrelease 8888:80"
+p "# Check out changes!"
+p "# Proxy http://localhost:8888 into pod"
+p "# Control-C to kill and continue"
+pe "$KUBECTL port-forward svc/wigm-host-yaml1 8888:80"
 
-# ----------------------------- Upgrade
-p "# Upgrade release 1. Enable ingress"
+# ----------------------------- Report configuration
+p "# Report configuration"
+pe "$KUBECTL get -f releases/yaml1.yaml"
 wait
 
-# ----------------------------- Break something
-p "# Accidentally break something"
-pe "$KUBECTL delete svc wigm-host-yamlrelease"
-wait
-
-# ----------------------------- Proxy
-p "# Proxy connection into a release 1 pod, Control-C to kill and continue"
-pe "$KUBECTL port-forward svc/wigm-host-yamlrelease 8888:80"
-wait
-
-# ----------------------------- cleanup
-p "# Demo done! Don't forget to clean up!"
-p "# Clean up all wigm releases"
-pe "$KUBECTL delete -f ./releases"
+# ----------------------------- Delete
+p "# Delete"
+pe "$KUBECTL delete -f ./releases/yaml1.yaml"
 wait
